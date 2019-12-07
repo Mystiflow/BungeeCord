3b2cabfa83452653ae6192ddd33eb668d365f497
Mystiflow <mystiflow@gmail.com>
2019-12-07 02:50:27 +0000

    Removed Entity Rewrite

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -115,7 +115,6 @@ public class DownstreamBridge extends PacketHandler
     @Override
     public void handle(PacketWrapper packet) throws Exception
     {
-        con.getEntityRewrite().rewriteClientbound( packet.buf, con.getServerEntityId(), con.getClientEntityId(), con.getPendingConnection().getVersion() );
         con.sendPacket( packet );
     }
 
--- proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/UpstreamBridge.java
@@ -113,7 +113,6 @@ public class UpstreamBridge extends PacketHandler
     {
         if ( con.getServer() != null )
         {
-            con.getEntityRewrite().rewriteServerbound( packet.buf, con.getClientEntityId(), con.getServerEntityId(), con.getPendingConnection().getVersion() );
             con.getServer().getCh().write( packet );
         }
     }
